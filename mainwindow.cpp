f5dfd53450d963ab1dc881f07452190902a0644a
crimvael <x.chn123@gmail.com>
2021-09-04 19:16:36 +0200

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QFile"
+#include "QJsonDocument"
+#include "QJsonObject"
+#include "QJsonArray"
 
 extern QString filepath;
 QString filepath = "C:/Users/Oscar/Desktop/ordine_tabacco/database.json";
@@ -9,6 +13,31 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QString val;
+    QFile file;
+    file.setFileName(filepath);
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+    val = file.readAll();
+    file.close();
+
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(val.toUtf8());
+    QJsonObject jsonObject = jsonResponse.object();
+    QJsonArray sigarette = jsonObject["elenco"].toArray();
+    QJsonArray sigaretti = jsonObject["SIGARETTI"].toArray();
+    QJsonArray tabacco = jsonObject["TABACCO"].toArray();
+
+    for (int i=0; i<sigarette.size(); i++) {
+        QJsonObject tmp = sigarette.at(i).toObject();
+        ui->tableWidget->insertRow(i);
+        QTableWidgetItem *item =  new QTableWidgetItem(tmp["nome"].toString());
+        item->setTextAlignment(Qt::AlignCenter);
+        ui->tableWidget->setItem(i, 0, item);
+    }
+
+    ui->tableWidget->resizeRowsToContents();
+    ui->tableWidget->resizeColumnsToContents();
+
 }
 
 MainWindow::~MainWindow()
