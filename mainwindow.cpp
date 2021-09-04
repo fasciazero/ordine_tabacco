8f3d452c13a937d1853dc1bc1571ff3a6585894a
crimvael <x.chn123@gmail.com>
2021-09-04 14:20:11 +0200

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+extern QString filepath;
+QString filepath = "C:/Users/Oscar/Desktop/ordine_tabacco/database.json";
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
