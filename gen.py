48b786cb54c9a1569e9fb7d9e310abcaf14e13e3
Oscar Chen <x.chn123@gmail.com>
2024-03-20 00:39:41 +0100

    update

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ import json
 file = pd.read_excel(r'C:\Users\Secernato\Documents\Qt projects\ordine_tabacco\ORDINE TABACCO.xlsx', sheet_name = None)
 file2 = open(r'C:\Users\Secernato\Documents\Qt projects\ordine_tabacco\database.json', "w")
 obj = {"elenco" : []}
-for row in range(0, 194):
+for row in range(0, 167):
     single_data = {
     "codice" : str(file["Foglio1"].iloc[row, 1]),
     "nome" : file["Foglio1"].iloc[row, 0],
